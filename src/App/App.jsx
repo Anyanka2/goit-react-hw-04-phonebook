33b4ad10d34069614554c40721331807e1a94dcb
Anyanka2 <123205369+Anyanka2@users.noreply.github.com>
2023-08-15 20:28:19 +0200

    wip

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -6,12 +6,7 @@ import { Container, Title, SubTitle, Wrapper } from './App.styled';
 import { nanoid } from 'nanoid';
 import Notiflix from 'notiflix';
 
-const phoneContacts = [
-  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-];
+
 
 //   componentDidMount() {
 //     const contacts = localStorage.getItem('contacts');
@@ -21,6 +16,12 @@ const phoneContacts = [
 //     }
 // }
 export const App = () => {
+  const phoneContacts = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+];
   const [contacts, setContacts] = useState(() => {
     return JSON.parse(window.localStorage.getItem('contacts')) ?? phoneContacts;
   });
@@ -59,7 +60,7 @@ export const App = () => {
     const isInContacts = contacts.some(
       ({ name, number }) =>
         name.toLowerCase() === contact.name.toLowerCase().trim() ||
-        contact.number === number
+        number === contact.number.trim()
     );
 
     if (isInContacts) {
@@ -81,12 +82,12 @@ export const App = () => {
     setFilter(event.target.value.trim());
   };
   const getVisibleContacts = () => {
-    const { filter, contacts } = this.state;
+    const normalizedFilter = filter.toLowerCase();
 
     return contacts.filter(
       contact =>
         contact.name.toLowerCase().includes(filter.toLowerCase()) ||
-        contact.number.includes(filter)
+        contact.number.includes(normalizedFilter)
     );
   };
   const visibleContacts = getVisibleContacts();
@@ -102,16 +103,14 @@ export const App = () => {
     return (
       <Container>
         <Title>Phonebook</Title>
-
         <ContactFrom onSubmit={addContact} />
-
         <SubTitle>Contacts</SubTitle>
-        {this.state.contacts.length > 0 ? (
+        {contacts.length > 0 ? (
           <Filter value={filter} onChangeFilter={changeFilter} />
         ) : (
           <Wrapper>Your phonebook is empty. Add first contact!</Wrapper>
         )}
-        {this.state.contacts.length > 0 && (
+        {contacts.length > 0 && (
           <ContactList
             contacts={visibleContacts}
             onRemoveContact={removeContact}
